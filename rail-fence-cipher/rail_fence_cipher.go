07f79085c073aca3d9b7dcb511b3485bd015b221
SultanOfSauce <massaria44@gmail.com>
2022-06-27 17:24:53 +0200

    commit in main

--- rail-fence-cipher/rail_fence_cipher.go
+++ rail-fence-cipher/rail_fence_cipher.go
@@ -1,24 +1,71 @@
 package railfence
 
+import (
+	"fmt"
+	"strings"
+)
+
 func Encode(message string, rails int) string {
-	panic("Please implement the Encode function")
+	fmt.Println(message, rails)
+
+	messageArray := []rune(message)
+
+	matrix := make([][]rune, rails)
+	for rail := range matrix {
+		matrix[rail] = make([]rune, len(messageArray))
+	}
+
+	for i, rail, direction := 0, 0, 1; i < len(message); i, rail = i+1, rail+direction {
+		matrix[rail][i] = messageArray[i]
+
+		if (rail + direction) == rails {
+			direction = -1
+		} else if (rail + direction) == -1 {
+			direction = 1
+		}
+	}
+
+	fmt.Println(matrix)
+
+	retStr := ""
+
+	for i := 0; i < rails; i++ {
+		retStr += string(matrix[i])
+	}
+
+	return strings.Replace(retStr, string("\x00"), "", -1)
+
 }
 
 func Decode(message string, rails int) string {
 
 	messageArray := []rune(message)
-	length := len(messageArray)
+	retArray := make([]rune, len(message))
 
-	decodedArray := make([]rune, length)
-	i := 0
+	cipher_char := 1
 
-	for rail := 0; rail < rails; rail++ {
-		for index := 0 + rail; index < length; index += 2 * (rails - rail - 1) {
-			decodedArray[index] = messageArray[i]
-			i++
+	c_len := len(message)
+
+	for i := 1; i <= rails; i++ {
+		flag := true
+
+		plain_char := i
+		for plain_char <= c_len {
+			retArray[plain_char-1] = messageArray[cipher_char-1]
+			cipher_char++
+
+			if i == 1 || i == rails {
+				plain_char = plain_char + (rails-1)*2
+			} else {
+				if flag {
+					plain_char = plain_char + (rails-i)*2
+					flag = false
+				} else {
+					plain_char = plain_char + ((i - 1) * 2)
+					flag = true
+				}
+			}
 		}
 	}
-
-	return string(messageArray)
-
+	return string(retArray)
 }
